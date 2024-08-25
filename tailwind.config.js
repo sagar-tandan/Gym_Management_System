fa4738c72c9afa70df3a193ed2470852837015f6
Sagar Tandan <076bct072@ioepc.edu.np>
2024-08-25 21:50:46 +0545

    trying

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   darkMode: "class", // Enable dark mode based on the 'dark' class
+
+  
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
