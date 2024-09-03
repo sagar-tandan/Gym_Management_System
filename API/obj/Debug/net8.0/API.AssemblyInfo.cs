e00987faeea395b3f5f1a0f46ca7325b38aa5335
Sagar Tandan <076bct072@ioepc.edu.np>
2024-09-03 13:30:31 +0545

    Added get

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fb922bb183b7c8d24f3cd61117da220d98f137ba")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+44e12a7fcce2c71eb5e66bd51cee2fb16e4fd98c")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
