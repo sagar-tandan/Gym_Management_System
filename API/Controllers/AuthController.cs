89018c855d919bd751decce9f4199f1619f0a8fc
Sagar Tandan <076bct072@ioepc.edu.np>
2024-09-10 14:22:22 +0545

    Authentication completed

---
+++ API/Controllers/AuthController.cs
@@ -0,0 +1,126 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.DTOs.AuthDTO;
+using API.Models;
+using API.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [Route("api/auth")]
+    [ApiController]
+
+
+    public class AuthController : ControllerBase
+    {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly TokenService _tokenService;
+        private readonly SignInManager<AppUser> _signinManager;
+
+        public AuthController(UserManager<AppUser> userManager, TokenService tokenService, SignInManager<AppUser> signInManager)
+        {
+            _userManager = userManager;
+            _tokenService = tokenService;
+            _signinManager = signInManager;
+
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login([FromBody] LoginDto loginDTO)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            };
+
+            var user = await _userManager.Users.FirstOrDefaultAsync(x => x.UserName == loginDTO.Username);
+            if (user == null) return Unauthorized("Invalid username!");
+            var result = await _signinManager.CheckPasswordSignInAsync(user, loginDTO.Password, false);
+
+            if (!result.Succeeded) return Unauthorized("Username not found and/or password incorrect");
+            var roles = await _userManager.GetRolesAsync(user);
+
+            return Ok(
+                new NewUserDTO
+                {
+                    Username = user.UserName,
+                    Email = user.Email,
+                    Token = _tokenService.CreateToken(user),
+                    UserId = user.Id,
+                    Roles = roles
+
+                }
+            );
+
+
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register([FromBody] RegisterDto registerDTO)
+        {
+            try
+            {
+
+                if (!ModelState.IsValid)
+                    return BadRequest(ModelState);
+
+                var appUser = new AppUser
+                {
+                    UserName = registerDTO.Username,
+                    Email = registerDTO.Email
+                };
+                var createdUser = await _userManager.CreateAsync(appUser, registerDTO.Password);
+
+                if (createdUser.Succeeded)
+                {
+                    var roleResult = await _userManager.AddToRoleAsync(appUser, "User");
+                    if (roleResult.Succeeded)
+                    {
+                        return Ok(
+                            new NewUserDTO
+                            {
+                                Username = appUser.UserName,
+                                Email = appUser.Email,
+                            }
+                        );
+                    }
+                    else
+                    {
+                        return StatusCode(500, roleResult.Errors);
+                    }
+                }
+                else
+                {
+                    return StatusCode(500, createdUser.Errors);
+                }
+            }
+            catch (Exception e)
+            {
+
+                return StatusCode(500, e);
+            }
+        }
+
+        [Authorize]
+        [HttpGet("verify-token")]
+        public async Task<IActionResult> VerifyToken()
+        {
+            // var userId = User.GetUserId();
+            // var user = await _userManager.FindByIdAsync(userId);
+            // if (user == null)
+            // {
+            //     return NotFound();
+            // }
+            // var roles = await _userManager.GetRolesAsync(user);
+
+            return Ok("Verified!!");
+        }
+
+
+    }
+}
--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f8b76bcae5332f3c7fad4d769de943ffcd39d596")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ca080d47987a13a484afa34e69c416f8c51ed786")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
