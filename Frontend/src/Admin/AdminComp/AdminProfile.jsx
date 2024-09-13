4c60b7b987f81194ed916a77e8fafccf8b23de76
Sagar Tandan <076bct072@ioepc.edu.np>
2024-09-13 16:00:17 +0545

    UI enhancement

--- Frontend/src/Admin/AdminComp/AdminProfile.jsx
+++ Frontend/src/Admin/AdminComp/AdminProfile.jsx
@@ -46,16 +46,20 @@ const AdminProfile = () => {
     }
   };
 
-  // const fetchDetails = async () => {
-  //   try {
-  //     const response = await axios.get(
-  //       "http://localhost:5002/api/auth/verify-token"
-  //     );
-  //   } catch (error) {}
-  // };
+  const fetchAllAdminDetails = async () => {
+    try {
+      const response = await axios.get(
+        "http://localhost:5002/api/auth/getAllAdmins"
+      );
+      setAllUser(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   useEffect(() => {
     getUidFromLocalStorage();
+    fetchAllAdminDetails();
   }, []);
 
   useEffect(() => {
@@ -87,12 +91,52 @@ const AdminProfile = () => {
           </div>
         </div>
 
-        <button className="absolute bottom-[-70px] right-[20px] px-1 py-[5px] border-[2px] border-[#b32fb1] w-[150px] rounded-md text-[#b32fb1] font-nunito text-[15px]">
-          Edit profile
-        </button>
+        <div className="absolute bottom-[-70px] right-[20px] flex gap-3">
+          <button className="px-1 py-[5px] border-[2px] border-[#b32fb1] w-[150px] rounded-sm text-[#b32fb1] font-nunito text-[15px]">
+            Edit profile
+          </button>
+          <button className="px-1 py-[5px] bg-purple-700 w-[150px] rounded-sm hover:bg-purple-900 transition-all duration-500 text-white font-nunito text-[15px]">
+            Add new Admin
+          </button>
+        </div>
       </div>
 
-      <div className="w-full bg-blue-500 p-4"></div>
+      {/* <div className="w-full bg-blue-500 p-4"></div> */}
+
+      <div className="w-full flex flex-col gap-2 h-screen">
+        {/* <div className="w-full flex justify-start px-3">
+          <button
+            onClick={(e) => MountModel(e)}
+            className="px-3 py-[6px] bg-purple-700 text-white rounded-sm hover:bg-purple-900 active:bg-purple-900 transition-all duration-300 ease-in-out font-medium "
+          >
+            Add new Admin
+          </button>
+        </div> */}
+        <section className="w-full py-1 px-3">
+          <table className="w-full">
+            <thead>
+              <tr className="w-full border-[1px] border-purple-200 bg-purple-100 rounded-sm ">
+                <th className="font-medium text-left pl-4 py-2 text-[#636363]">
+                  S.No.
+                </th>
+                <th className="font-medium text-left pl-4 py-2 text-[#636363]">
+                  Username
+                </th>
+                <th className="font-medium text-left pl-4 py-2 text-[#636363]">
+                  Email
+                </th>
+                <th className="font-medium text-left pl-4 py-2 text-[#636363]">
+                  Role
+                </th>
+
+                <th className="font-medium text-left pl-4 py-2 text-[#636363]">
+                  Actions
+                </th>
+              </tr>
+            </thead>
+          </table>
+        </section>
+      </div>
     </div>
   );
 };
