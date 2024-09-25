c8c113c3c23e1edb48441954f0013b401235221b
Sagar Tandan <076bct072@ioepc.edu.np>
2024-09-25 15:08:39 +0545

    Done fro today and some issue fixed

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -34,7 +34,6 @@ function App() {
   return (
     <>
       <BrowserRouter>
-        <Navbar />
         <Routes>
           <Route path="/" element={<UserPage />} />
           <Route
--- Frontend/src/User/UserPage.jsx
+++ Frontend/src/User/UserPage.jsx
@@ -8,7 +8,11 @@ const UserPage = () => {
   return (
     <div className="w-full overflow-x-hidden relative">
       <div className="fixed top-0 -z-10 h-full w-full max-w-screen-2xl">
-        <div class="absolute top-0 z-[-2] h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
+        <div class="relative h-full w-full bg-slate-950">
+          <div class="absolute bottom-0 left-0 right-0 top-0 bg-[radial-gradient(circle_500px_at_50%_200px,#3e3e3e,transparent)]"></div>
+        </div>
+
+        {/* <div class="absolute top-0 z-[-2] h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div> */}
       </div>
       <div className="w-full flex flex-col max-w-screen-2xl mx-auto">
         <HeroSection />
